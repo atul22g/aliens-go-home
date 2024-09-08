69108f8a664c5514a1ab9068dc89d1c043c8c0c0
Atul <atul22g2468@gmail.com>
2024-09-08 13:43:59 +0530

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,45 @@
+import {MOVE_OBJECTS, START_GAME, LEADERBOARD_LOADED, LOGGED_IN, SHOOT,} from '../actions';
+import moveObjects from './moveObjects';
+import startGame from './startGame';
+import shoot from './shoot';
+
+const initialGameState = {
+	started: false,
+	kills: 0,
+	lives: 3,
+	flyingObjects: [],
+	lastObjectCreatedAt: new Date(),
+	currentPlayer: null,
+	players: null,
+	cannonBalls: [],
+};
+
+const initialState = {
+	angle: 45,
+	gameState: initialGameState,
+};
+
+function reducer(state = initialState, action) {
+	switch (action.type) {
+		case MOVE_OBJECTS:
+			return moveObjects(state, action);
+		case START_GAME:
+			return startGame(state, initialGameState);
+		case LEADERBOARD_LOADED:
+			return {
+				...state,
+				players: action.players,
+			};
+		case LOGGED_IN:
+			return {
+				...state,
+				currentPlayer: action.player,
+			};
+		case SHOOT:
+			return shoot(state, action);
+		default:
+			return state;
+	}
+}
+
+export default reducer;
---
+++ src/utils/constants.js
@@ -0,0 +1,19 @@
+// очень широко, чтобы наверняка занять весь экран
+export const skyAndGroundWidth = 5000;
+
+export const gameWidth = 800;
+
+export const createInterval = 1000;
+
+export const maxFlyingObjects = 4;
+
+export const flyingObjectsStarterYAxis = -1100;
+
+export const flyingObjectsStarterPositions = [
+	-300,
+	-150,
+	150,
+	300,
+];
+
+export const gameHeight = 1200;
